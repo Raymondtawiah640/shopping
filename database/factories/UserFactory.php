9bceacfade1fe5bc63ccc46bd4a6d84946fd977b
Raymondtawiah640 <enterprisekiln@gmail.com>
2026-01-12 11:17:44 -0900

    modi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,15 +22,15 @@ class UserFactory extends Factory
      * @return array<string, mixed>
      */
    public function definition(): array
-    {
-        return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
+{
+    return [
+        'name' => $this->faker->name(),
+        'email' => $this->faker->unique()->safeEmail(),
+        'email_verified_at' => now(),
+        'password' => static::$password ??= Hash::make('password'),
+        'remember_token' => Str::random(10),
+    ];
+}
 
 
     /**
