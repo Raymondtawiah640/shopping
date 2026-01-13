8fff7f2aea12cab5d5d1eb88ad0c55d7a9ea9648
Raymondtawiah640 <enterprisekiln@gmail.com>
2026-01-12 15:00:02 -0900

    Change the default credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
+            'name' => 'Kiln enterprise',
+            'email' => 'raymondtawiah23@gmail.com',
+            'password' => Hash::make('real'),
         ]);
     }
 }
