f747e10f1c835a596b07179270a15c19afd36790
Raymondtawiah640 <enterprisekiln@gmail.com>
2026-01-15 14:27:13 -0900

    modified

--- app/Mail/CustomerBookingConfirmation.php
+++ app/Mail/CustomerBookingConfirmation.php
@@ -12,7 +12,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class CustomerBookingConfirmation extends Mailable
+class CustomerBookingConfirmation extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
